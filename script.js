102f6f8c23aca1404fcabaf55a0af070b8bc88a7
Rebecca Naomi Naomi <26445706-Rebecca-NaomiNa@users.noreply.replit.com>
2024-05-30 15:18:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".title").hide();
+$(".title").delay(500);
+$(".title").fadeIn(1000);
+
+$("#recitalpic").hide();
+$("#recitalpic").delay(500);
+$("#recitalpic").fadeIn(1000);
+
+$("#firstpara").hide();
+$("#firstpara").delay(500);
+$("#firstpara").fadeIn(1000);
